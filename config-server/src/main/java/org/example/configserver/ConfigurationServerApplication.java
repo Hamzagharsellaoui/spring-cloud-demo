9b1025b814b4e06375cbe3c14f4ab6975339614d
HamzaGharsellaoui <hamza.gharsellaoui@enis.tn>
2025-07-29 13:58:29 +0100

    docker compose almost done

--- config-server/src/main/java/org/example/configserver/ConfigurationServerApplication.java
+++ config-server/src/main/java/org/example/configserver/ConfigurationServerApplication.java
@@ -3,14 +3,12 @@ package org.example.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author Olga Maciaszek-Sharma
  */
 @SpringBootApplication
 @EnableConfigServer
-@EnableEurekaServer
 public class ConfigurationServerApplication {
 
 	public static void main(String[] args) {
